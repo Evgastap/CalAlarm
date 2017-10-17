47766560ed0909d804409f6a9f7196795f4af1bd
Evgastap <evgastap@gmail.com>
2017-10-17 18:26:28 +0200

    first commit

---
+++ app/src/main/java/com/example/evgastap/calendaralarm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.evgastap.calendaralarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //if you synced with git you should see this asdf
+    }
+}
