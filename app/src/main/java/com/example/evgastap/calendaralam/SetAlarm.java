e71d0a2bb27c25a469d0e3fe2a81a52e1ad599d0
Evgastap <evgastap@gmail.com>
2017-10-19 11:38:32 +0200

    SetAlarm class created

---
+++ app/src/main/java/com/example/evgastap/calendaralam/SetAlarm.java
@@ -0,0 +1,14 @@
+package com.example.evgastap.calendaralam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetAlarm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_alarm);
+        //setalarm code goes here
+    }
+}
--- app/src/main/java/com/example/evgastap/calendaralarm/MainActivity.java
+++ app/src/main/java/com/example/evgastap/calendaralarm/MainActivity.java
@@ -1,7 +1,57 @@
 package com.example.evgastap.calendaralarm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.ExponentialBackOff;
+
+import com.google.api.services.calendar.CalendarScopes;
+import com.google.api.client.util.DateTime;
+
+import com.google.api.services.calendar.model.*;
+
+import android.Manifest;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+import android.widget.Button;
+
+import static android.R.attr.button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,9 +59,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //if you synced with git you should see this asdf
+        //if you synced with git you should see this
         //second comment for testing
         //another change!!!
+
+
+
+        final Button buttonGetEvents = (Button) findViewById(R.id.button_GetEvents);
+        buttonGetEvents.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    protected void getCalendarInfo() {
+        //Initialize Calendar service with valid OAuth credentials
+        Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
+                .setApplicationName("applicationName").build();
 
     }
 }
